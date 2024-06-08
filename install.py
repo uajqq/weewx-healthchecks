6ec03db4205e29fb6d487bc822569cbd0c9cdb2f
uajqq <46248396+uajqq@users.noreply.github.com>
2024-06-08 14:15:26 -0400

    Update install.py for weewx 5.0

--- install.py
+++ install.py
@@ -1,40 +1,23 @@
-# Installer file for Healthchecks.io extension for WeeWX
-# Distributed under the terms of the GNU Public License (GPLv3)
+# installer for weewx-healthchecks
+# Copyright 2024 uajqq
 
-from bin.user.healthchecks import Healthchecks
-import configobj
-from setup import ExtensionInstaller
-
-try:
-    # Python 2
-    from StringIO import StringIO
-except ImportError:
-    # Python 3
-    from io import StringIO
-
+from weecfg.extension import ExtensionInstaller
 
 def loader():
     return healthchecksInstaller()
 
-
 class healthchecksInstaller(ExtensionInstaller):
     def __init__(self):
         super(healthchecksInstaller, self).__init__(
-            version="1.0",
-            name="Healthchecks",
-            description="Send pings to healthchecks.io for monitoring purposes",
+            version="1.1",
+            name='Healthchecks',
+            description='Send pings to healthchecks.io for monitoring purposes',
             author="uajqq",
             report_services="user.healthchecks.Healthchecks",
-            config=config_dict,
-            files=[("bin/user", ["bin/user/healthchecks.py"])],
+            config={
+                'Healthchecks': {
+                    'url': '<replace with custom URL>',
+                },
+            },
+            files=[('bin/user', ['bin/user/healthchecks.py'])],
         )
-
-
-# Config stanza
-extension_config = """
-
-[Healthchecks]
-    url = <insert custom url>
-
-"""
-config_dict = configobj.ConfigObj(StringIO(extension_config))
